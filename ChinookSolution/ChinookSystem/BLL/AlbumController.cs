be35632965bf47b07ecfaf4493a8dbab467bf06c
Don Welch <dwelch@nait.ca>
2020-01-10 16:44:53 -0700

    review of basic BLL controller method with methods List and Find

---
+++ ChinookSolution/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.Data.Entities;
+using ChinookSystem.DAL;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    public class AlbumController
+    {
+        //basic query: complete list of DbSet
+        public List<Album> Album_List()
+        {
+            //set up the code block to ensure the release of the sql connection
+            using (var context = new ChinookContext())
+            {
+                //.ToList<T> is used to convert the DbSet<T> into a List<T> collection
+                return context.Albums.ToList();
+            }
+
+        }
+
+        //basic query: return a recorded based on pkey
+        public Album Album_FindByID(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.Find(albumid);
+            }
+        }
+    }
+}
---
+++ ChinookSolution/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+#region Additional Namespaces
+using ChinookSystem.Data.Entities;
+using ChinookSystem.DAL;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    public class ArtistController
+    {
+        //basic query: complete list of DbSet
+        public List<Artist> Artist_List()
+        {
+            //set up the code block to ensure the release of the sql connection
+            using(var context = new ChinookContext())
+            {
+                //.ToList<T> is used to convert the DbSet<T> into a List<T> collection
+                return context.Artists.ToList();
+            }
+
+        }
+
+        //basic query: return a recorded based on pkey
+        public Artist Artist_FindByID (int artistid)
+        {
+            using(var context = new ChinookContext())
+            {
+                return context.Artists.Find(artistid);
+            }
+        }
+    }
+}
