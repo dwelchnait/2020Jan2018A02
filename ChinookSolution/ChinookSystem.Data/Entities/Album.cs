8ac0c738ed0a572a40e0316200a4c0c6429ed5c7
Don Welch <dwelch@nait.ca>
2020-01-10 16:18:50 -0700

    complete review of enitities and introductiion of navigational properties

---
+++ ChinookSolution/ChinookSystem.Data/Entities/Album.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("Albums")]
+    public class Album
+    {
+        private string _ReleaseLabel;
+
+        [Key]
+        public int AlbumId { get; set; }
+        [Required(ErrorMessage ="Album title is required.")]
+        [StringLength(160, ErrorMessage ="Album title is limited to 160 characters")]
+        public string AlbumTitle { get; set; }
+       // [Required(ErrorMessage ="Artist Id is required")]
+        [Range(1,int.MaxValue,ErrorMessage =("Artist Id out of range"))]
+        public int ArtistId { get; set; }
+
+        public int ReleaseYear { get; set; }
+        [StringLength(50, ErrorMessage ="Album release label is limited to 50 characters")]
+        public string ReleaseLabel
+        {
+            get
+            {
+                return _ReleaseLabel;
+            }
+            set
+            {
+                _ReleaseLabel = string.IsNullOrEmpty(value) ? null : value;
+            }
+        }
+
+        //notmapped properties
+        [NotMapped]
+        public string ReleaseInfo
+        {
+            get
+            {
+                return string.IsNullOrEmpty(ReleaseLabel) ? ReleaseYear + " (unknown)" :
+                    ReleaseYear + " (" + ReleaseLabel + ")";
+            }
+        }
+
+        //navigational properties
+        public virtual Artist Artist { get; set; }
+        //public virtual ICollection<Track> Tracks { get; set; }
+    }
+}
---
+++ ChinookSolution/ChinookSystem.Data/Entities/Artist.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    //identify the sql entity (table) this class maps
+    [Table("Artists")]
+    public class Artist
+    {
+        private string _Name;
+
+        //check your sql entity for type of pkey (identity or other)
+        [Key]
+        public int ArtistId { get; set; }
+
+        //fully implement nullable strings (course standard)
+        //check if the sql entity attribute has any constraints
+        [StringLength(120, ErrorMessage ="Artist Name is limited to 120 characters")]
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                //if (string.IsNullOrEmpty(value))
+                //{
+                //    _Name = null;
+                //}
+                //else
+                //{
+                //    _Name = value;
+                //}
+
+                _Name = string.IsNullOrEmpty(value) ? null : value;
+            }
+        }
+
+        //[NotMapped] properties
+
+        //navigational properties
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+}
