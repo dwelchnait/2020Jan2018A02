5c1202d97c4f22be7ef77aaefe4e240143cc393a
Don Welch <dwelch@nait.ca>
2020-01-21 09:54:32 -0700

    Reverse Engineer Entities

--- ChinookSolution/ChinookSystem.Data/Entities/Album.cs
+++ ChinookSolution/ChinookSystem.Data/Entities/Album.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-#region Additional Namespaces
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-#endregion
-
 namespace ChinookSystem.Data.Entities
 {
-    [Table("Albums")]
-    public class Album
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Album
     {
         private string _ReleaseLabel;
 
-        [Key]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Album()
+        {
+            Tracks = new HashSet<Track>();
+        }
+
         public int AlbumId { get; set; }
-        [Required(ErrorMessage ="Album title is required.")]
-        [StringLength(160, ErrorMessage ="Album title is limited to 160 characters")]
+
+        [Required(ErrorMessage ="Title is required")]
+        [StringLength(160, ErrorMessage ="Title is limited to 160 characters")]
         public string Title { get; set; }
-       // [Required(ErrorMessage ="Artist Id is required")]
-        [Range(1,int.MaxValue,ErrorMessage =("Artist Id out of range"))]
+
         public int ArtistId { get; set; }
 
         public int ReleaseYear { get; set; }
-        [StringLength(50, ErrorMessage ="Album release label is limited to 50 characters")]
+
+        [StringLength(50, ErrorMessage = "Release label is limited to 50 characters")]
         public string ReleaseLabel
         {
             get
@@ -39,19 +39,19 @@ namespace ChinookSystem.Data.Entities
             }
         }
 
-        //notmapped properties
         [NotMapped]
         public string ReleaseInfo
         {
             get
             {
-                return string.IsNullOrEmpty(ReleaseLabel) ? ReleaseYear + " (unknown)" :
+                return string.IsNullOrEmpty(_ReleaseLabel) ? "Unknown" :
                     ReleaseYear + " (" + ReleaseLabel + ")";
             }
         }
 
-        //navigational properties
         public virtual Artist Artist { get; set; }
-        //public virtual ICollection<Track> Tracks { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Track> Tracks { get; set; }
     }
 }
