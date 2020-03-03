f2d24eb0e973718ddd7aecad2aae5403bfb3be1b
Don Welch <dwelch@nait.ca>
2020-03-03 09:52:15 -0700

    Install start kit for ManagePlaylist page. Implementation of filter queries for Track list

--- ChinookSolution/ChinookSystem/BLL/TrackController.cs
+++ ChinookSolution/ChinookSystem/BLL/TrackController.cs
@@ -17,14 +17,88 @@ namespace ChinookSystem.BLL
     [DataObject]
     public class TrackController
     {
-        [DataObjectMethod(DataObjectMethodType.Select,false)]
-        public List<Track> Get_TracksForPlaylistSelection(int id, string fetchby)
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<TrackList> List_TracksForPlaylistSelection(string tracksby, string arg)
         {
-            using(var context = new ChinookContext())
+            using (var context = new ChinookContext())
             {
-                var results = from x in context.Tracks
-                              where x.AlbumId == id
-                              select x;
+                List<TrackList> results = null;
+                int id = 0;
+                if (int.TryParse(arg, out id))
+                {
+                    //lookup for MediaType or Genre
+                    results = (from x in context.Tracks
+                               where (tracksby.Equals("MediaType") && x.MediaTypeId == id)
+                                 || (tracksby.Equals("Genre") && x.GenreId == id)
+                               orderby x.Name
+                               select new TrackList
+                               {
+                                   TrackID = x.TrackId,
+                                   Name = x.Name,
+                                   Title = x.Album.Title,
+                                   ArtistName = x.Album.Artist.Name,
+                                   MediaName = x.MediaType.Name,
+                                   GenreName = x.Genre.Name,
+                                   Composer = x.Composer,
+                                   Milliseconds = x.Milliseconds,
+                                   Bytes = x.Bytes,
+                                   UnitPrice = x.UnitPrice
+                               }).ToList();
+                }
+                else
+                {
+                    //lookup for artist or album
+                    results = (from x in context.Tracks
+                               where (tracksby.Equals("Artist") && x.Album.Artist.Name.Contains(arg))
+                                 || (tracksby.Equals("Album") && x.Album.Title.Contains(arg))
+                               orderby x.Name
+                               select new TrackList
+                               {
+                                   TrackID = x.TrackId,
+                                   Name = x.Name,
+                                   Title = x.Album.Title,
+                                   ArtistName = x.Album.Artist.Name,
+                                   MediaName = x.MediaType.Name,
+                                   GenreName = x.Genre.Name,
+                                   Composer = x.Composer,
+                                   Milliseconds = x.Milliseconds,
+                                   Bytes = x.Bytes,
+                                   UnitPrice = x.UnitPrice
+                               }).ToList();
+                }
+              
+                        
+
+                return results;
+            }
+        }//eom
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Track> Track_List()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Tracks.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public Track Track_Find(int trackid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Tracks.Find(trackid);
+            }
+        }
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Track> Track_GetByAlbumId(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                var results = from aRowOn in context.Tracks
+                              where aRowOn.AlbumId.HasValue
+                              && aRowOn.AlbumId == albumid
+                              select aRowOn;
                 return results.ToList();
             }
         }
---
+++ ChinookSolution/WebApp/SamplePages/ManagePlaylist.aspx.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+#region Additonal Namespaces
+using ChinookSystem.BLL;
+using ChinookSystem.Data.POCOs;
+#endregion
+
+namespace WebApp.SamplePages
+{
+    public partial class ManagePlaylist : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TracksSelectionList.DataSource = null;
+        }
+
+        protected void CheckForException(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            MessageUserControl.HandleDataBoundException(e);
+        }
+
+        protected void ArtistFetch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ArtistName.Text))
+            {
+                //message to the user
+                MessageUserControl.ShowInfo("Entry Error", "Select an artist name or part of.");
+            }
+            else
+            {
+                TracksBy.Text = "Artist";
+                SearchArg.Text = ArtistName.Text;
+                TracksSelectionList.DataBind();
+            }
+        }
+
+        protected void MediaTypeDDL_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (MediaTypeDDL.SelectedIndex == 0)
+            {
+                //message to the user
+                MessageUserControl.ShowInfo("Selection Error", "Enter a media type");
+            }
+            else
+            {
+                TracksBy.Text = "MediaType";
+                SearchArg.Text = MediaTypeDDL.SelectedValue;
+                TracksSelectionList.DataBind();
+            }
+        }
+
+        protected void GenreFetch_Click(object sender, EventArgs e)
+        {
+            TracksBy.Text = "Genre";
+            SearchArg.Text = GenreDDL.SelectedValue;
+            TracksSelectionList.DataBind();
+            
+        }
+
+        protected void AlbumFetch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(AlbumTitle.Text))
+            {
+                //message to the user
+                MessageUserControl.ShowInfo("Entry Error", "Enter an album title or part of.");
+            }
+            else
+            {
+                TracksBy.Text = "Album";
+                SearchArg.Text = AlbumTitle.Text;
+                TracksSelectionList.DataBind();
+            }
+        }
+
+        protected void PlayListFetch_Click(object sender, EventArgs e)
+        {
+            //code to go here
+ 
+        }
+
+        protected void MoveDown_Click(object sender, EventArgs e)
+        {
+            //code to go here
+ 
+        }
+
+        protected void MoveUp_Click(object sender, EventArgs e)
+        {
+            //code to go here
+ 
+        }
+
+        protected void MoveTrack(int trackid, int tracknumber, string direction)
+        {
+            //call BLL to move track
+ 
+        }
+
+
+        protected void DeleteTrack_Click(object sender, EventArgs e)
+        {
+            //code to go here
+ 
+        }
+
+        protected void TracksSelectionList_ItemCommand(object sender, 
+            ListViewCommandEventArgs e)
+        {
+            //code to go here
+            
+        }
+
+
+    }
+}
